744f818acb41ba8c0f322924de42c09ba61dfc9c
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-01-22 13:29:35 -0800

    feat(niri): init ws comp

--- src/niri/mod.rs
+++ src/niri/mod.rs
@@ -1,122 +1,28 @@
-use std::{env::var_os, ffi::OsString, time::Duration};
+use iced::Subscription;
+use stream::NiriEvent;
+use tracing::info;
 
-use iced::futures::{Stream, stream};
-use niri_ipc::{Event as NiriEvent, Reply, Request, socket};
-use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::UnixStream,
-};
 
-pub mod types;
+mod stream;
+mod types;
 
-#[derive(Debug, thiserror::Error)]
-pub enum NiriStreamError {
-    #[error("No socket found for Niri")]
-    NiriNoSocket,
-
-    #[error("Failed to connect to socket")]
-    NiriConnectionError(#[from] std::io::Error),
-
-    #[error("Niri refused connection")]
-    NiriConnectionClosed,
-
-    #[error("Niri refused event stream")]
-    NiriStreamRefused(String),
-
-    #[error("Serde failed to parse")]
-    SerdeErr(#[from] serde_json::Error),
+pub enum Message {
+    Noop(NiriEvent),
 }
 
-enum NiriStream {
-    Disconnected { attempts: u32 },
-    Connected(BufReader<UnixStream>),
-}
+pub struct NiriWS {}
 
-impl NiriStream {
-    fn path() -> Result<OsString, NiriStreamError> {
-        var_os(socket::SOCKET_PATH_ENV).ok_or(NiriStreamError::NiriNoSocket)
+
+impl NiriWS {
+    fn new() -> Self {
+        Self {}
     }
-
-    pub async fn new() -> Result<BufReader<UnixStream>, NiriStreamError> {
-        let path = Self::path()?;
-        let req_buff = serde_json::to_string(&Request::EventStream)? + "\n";
-
-        let mut stream = UnixStream::connect(path).await?;
-
-        stream.writable().await?;
-        stream.write_all(req_buff.as_bytes()).await?;
-        stream.shutdown().await?;
-
-        let mut reader = BufReader::new(stream);
-
-        let mut line = String::new();
-        reader.read_line(&mut line).await?;
-
-        let reply: Reply = serde_json::from_str(&line)?;
-        reply.map_err(NiriStreamError::NiriStreamRefused)?;
-
-        Ok(reader)
+    fn update(&mut self) {}
+    fn subscription(&self) -> Subscription<Message> {
+        Subscription::run(stream::listen).filter_map(|event| {
+            info!("niri event {event:?}");
+            None
+        })
     }
-
-    async fn read(reader: &mut BufReader<UnixStream>) -> Result<NiriEvent, NiriStreamError> {
-        let mut line = String::new();
-        let bytes_read = reader.read_line(&mut line).await?;
-        if bytes_read == 0 {
-            return Err(NiriStreamError::NiriConnectionClosed);
-        }
-
-        serde_json::from_str::<NiriEvent>(&line).map_err(NiriStreamError::SerdeErr)
-    }
-}
-
-pub fn listen() -> impl Stream<Item = Result<NiriEvent, NiriStreamError>> {
-    let eventstream = NiriStream::Disconnected { attempts: 0 };
-    stream::unfold(eventstream, |es| async move {
-        match es {
-            NiriStream::Disconnected { attempts } => {
-                if attempts >= 4 {
-                    return None;
-                }
-
-                if attempts > 0 {
-                    let duration = Duration::from_secs(1 * 2_u64.pow(attempts));
-                    tokio::time::sleep(duration).await;
-                }
-
-                let mut reader = match NiriStream::new().await {
-                    Ok(reader) => reader,
-                    Err(err) => {
-                        return Some((
-                            Err(err),
-                            NiriStream::Disconnected {
-                                attempts: attempts + 1,
-                            },
-                        ));
-                    }
-                };
-
-                let event = match NiriStream::read(&mut reader).await {
-                    Ok(event) => event,
-                    Err(err) => {
-                        return Some((
-                            Err(err),
-                            NiriStream::Disconnected {
-                                attempts: attempts + 1,
-                            },
-                        ));
-                    }
-                };
-
-                Some((Ok(event), NiriStream::Connected(reader)))
-            }
-            NiriStream::Connected(mut reader) => {
-                let event = match NiriStream::read(&mut reader).await {
-                    Ok(event) => event,
-                    Err(err) => return Some((Err(err), NiriStream::Disconnected { attempts: 1 })),
-                };
-
-                Some((Ok(event), NiriStream::Connected(reader)))
-            }
-        }
-    })
+    fn view() {}
 }
