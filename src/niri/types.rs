272efc3044d346fa4af2bedb5e05a2a6391ab45e
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-01-24 12:07:48 -0800

    feat(niri): impl window state

--- src/niri/types.rs
+++ src/niri/types.rs
@@ -1,18 +1,15 @@
+use derive_more::From;
 use niri_ipc::Event;
-use std::{collections::{BTreeMap, HashMap}, hash::Hash};
+use std::{
+    collections::{BTreeMap, HashMap},
+    hash::Hash,
+};
 
-#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+#[derive(Debug, Clone, Eq, Hash, PartialEq, From)]
 pub struct WorkspaceId(u64);
 
-#[derive(Debug, Clone, Eq, Hash, PartialEq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Eq, Hash, PartialEq, PartialOrd, Ord, From)]
 pub struct WorkspaceIdx(u8);
-
-impl From<&niri_ipc::Workspace> for WorkspaceIdx {
-    fn from(ws: &niri_ipc::Workspace) -> Self {
-        Self(ws.idx)
-    }
-}
-
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Workspace {
@@ -26,7 +23,7 @@ pub struct Workspace {
 impl<'a> From<&'a niri_ipc::Workspace> for Workspace {
     fn from(ws: &'a niri_ipc::Workspace) -> Self {
         Self {
-            id: WorkspaceId(ws.id),
+            id: ws.id.into(),
             monitor_id: ws.output.as_ref().map(|output| MonitorId(output.clone())),
             is_active: ws.is_active,
             is_urgent: ws.is_urgent,
@@ -36,10 +33,41 @@ impl<'a> From<&'a niri_ipc::Workspace> for Workspace {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct WsMap(BTreeMap<WorkspaceIdx, Workspace>);
+pub struct WsMap(BTreeMap<WorkspaceIdx, Workspace>);
 
 #[derive(Debug, Clone, PartialEq)]
-struct WsIdxMap(HashMap<WorkspaceId, WorkspaceIdx>);
+pub struct WsIdxMap(HashMap<WorkspaceId, WorkspaceIdx>);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, From)]
+pub struct WinId(u64);
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Window {
+    pub id: WinId,
+    pub title: Option<String>,
+    pub app_id: Option<String>,
+    pub ws_id: Option<WorkspaceId>,
+    pub is_focus: bool,
+    pub is_urgent: bool,
+    pub is_floating: bool,
+}
+
+impl<'a> From<&'a niri_ipc::Window> for Window {
+    fn from(value: &'a niri_ipc::Window) -> Self {
+        Self {
+            id: value.id.into(),
+            title: value.title.clone(),
+            ws_id: value.workspace_id.map(|id| id.into()),
+            app_id: value.app_id.clone(),
+            is_urgent: value.is_urgent,
+            is_focus: value.is_focused,
+            is_floating: value.is_floating,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct WinMap(HashMap<WinId, Window>);
 
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
 pub struct MonitorId(String);
@@ -53,6 +81,7 @@ pub struct State {
     // pub outputs: HashMap<MonitorId, Monitor>,
     pub ws_map: WsMap,
     pub ws_idx_map: WsIdxMap,
+    pub win_map: WinMap,
 }
 
 impl State {
@@ -62,24 +91,31 @@ impl State {
                 let state = Self {
                     ws_map: WsMap(BTreeMap::new()),
                     ws_idx_map: WsIdxMap(HashMap::new()),
+                    win_map: self.win_map,
                 };
                 workspaces.iter().fold(state, |mut state, ws| {
                     let my_ws = Workspace::from(ws);
-                    let idx = WorkspaceIdx::from(ws);
+                    let idx = WorkspaceIdx::from(ws.idx);
 
                     state.ws_idx_map.0.insert(my_ws.id.clone(), idx.clone());
                     state.ws_map.0.insert(idx.clone(), my_ws);
 
                     state
                 })
-            },
+            }
+            Event::WindowsChanged { windows } => {
+                let state = Self {
+                    ws_map: self.ws_map,
+                    ws_idx_map: self.ws_idx_map,
+                    win_map: WinMap(HashMap::new()),
+                };
+                windows.iter().fold(state, |mut state, niri_win| {
+                    let win = Window::from(niri_win);
+                    state.win_map.0.insert(win.id.clone(), win);
+                    state
+                })
+            }
             _ => self,
         }
     }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Events {
-    FocusedMonitor(MonitorId),
-    FocusedWorkspace(WorkspaceId),
 }
