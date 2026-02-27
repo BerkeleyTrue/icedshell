5fd4115790cc5ac737a73ed18f3dc003e15539f7
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-02-26 19:16:32 -0800

    feat(tray/menu): use stacking instead of columns for sub menus

--- src/tray/menu_comp.rs
+++ src/tray/menu_comp.rs
@@ -19,7 +19,8 @@ use crate::{
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    ToggleMenu(TrayMenuItemId),
+    OpenSubMenu(TrayMenuItemId),
+    CloseSubMenu,
     ItemSelected(String, TrayMenuItemId),
 }
 
@@ -32,77 +33,28 @@ pub struct MenuComp {
     name: String,
     layout: TrayLayout,
     theme: AppTheme,
-    open_menus: Vec<TrayMenuItemId>,
-}
-
-impl Comp for MenuComp {
-    type Message = Message;
-    type Init = Init;
-
-    fn new(input: Self::Init) -> Self {
-        let theme = app_theme();
-        let layout = &input.layout;
-        info!("layout: {layout:?}");
-        Self {
-            name: input.name,
-            layout: input.layout,
-            open_menus: Vec::new(),
-            theme,
-        }
-    }
-
-    fn update(&mut self, message: Self::Message) -> iced::Task<Self::Message> {
-        match message {
-            Message::ToggleMenu(id) => {
-                if self.open_menus.contains(&id) {
-                    self.open_menus.retain(|inner_id| *inner_id != id);
-                } else {
-                    self.open_menus.push(id);
-                }
-                Task::none()
-            }
-            Message::ItemSelected(_name, _id) => Task::none(),
-        }
-    }
-
-    fn view(&self) -> iced::Element<'_, Self::Message> {
-        let theme = &self.theme;
-        // top level layout is always submenu
-        let top_menu = self
-            .layout
-            .children
-            .iter()
-            .map(|menu| {
-                Element::from(
-                    container(self.view_menu(&self.name, menu))
-                        .padding(padding::left(theme.spacing().xs()))
-                        .center_y(theme.spacing().lg())
-                        .center_x(Length::Fill),
-                )
-            })
-            .fold(Column::new(), |col, item_elem| col.push(item_elem));
-
-        container(top_menu)
-            .height(Length::Fill)
-            .width(Length::Fill)
-            .padding(theme.spacing().xs())
-            .style(|_| container::Style {
-                border: border::rounded(theme.radius().lg())
-                    .color(theme.secondary(Shade::S500))
-                    .width(theme.spacing().xxs()),
-                background: Some(theme.neutral(Shade::S900).into()),
-                ..Default::default()
-            })
-            .into()
-    }
+    menu_stack: Vec<TrayMenuItemId>,
 }
 
 impl MenuComp {
+    fn get_current_menu<'a>(
+        stack: &'a [TrayMenuItemId],
+        layout: &'a TrayLayout,
+    ) -> &'a [TrayLayout] {
+        if let Some((item_id, rest)) = stack.split_first()
+            && let Some(next_children) = layout.children.iter().find(|id| id.id == *item_id)
+        {
+            Self::get_current_menu(rest, next_children)
+        } else {
+            &layout.children
+        }
+    }
+
     fn view_menu<'a>(&'a self, name: &'a str, layout: &'a TrayLayout) -> Element<'a, Message> {
         let theme = &self.theme;
         match &layout.props {
             // Divider
-            TrayLayoutProps { type_: Some(t), .. } if t == "seperator" => {
+            TrayLayoutProps { type_: Some(t), .. } if t == "separator" => {
                 // info!("sep");
                 container(text!("---")).into()
             }
@@ -128,15 +80,11 @@ impl MenuComp {
                 label: Some(label),
                 ..
             } if display == "submenu" => {
-                let is_open = self.open_menus.contains(&layout.id);
-                let icon = if is_open {
-                    Icon::DiamondMinus.widget()
-                } else {
-                    Icon::DiamondPlus.widget()
-                }
-                .size(theme.spacing().md())
-                .align_y(Vertical::Center)
-                .width(theme.spacing().lg());
+                let icon = Icon::DiamondPlus
+                    .widget()
+                    .size(theme.spacing().md())
+                    .align_y(Vertical::Center)
+                    .width(theme.spacing().lg());
 
                 let label = text(label.clone().replace("_", ""))
                     .align_y(Vertical::Center)
@@ -146,41 +94,25 @@ impl MenuComp {
                     .spacing(theme.spacing().xs())
                     .align_y(Vertical::Center);
 
-                let content = Column::new().push(
-                    button(button_content)
-                        .style(|_, status| {
-                            let base = button::Style {
-                                background: Some(Color::TRANSPARENT.into()),
-                                text_color: TEXT,
-                                ..Default::default()
-                            };
-                            match status {
-                                button::Status::Hovered => button::Style {
-                                    background: Some(SURFACE0.into()),
-                                    ..base
-                                },
-                                _ => base,
-                            }
-                        })
-                        .padding(
-                            padding::vertical(theme.spacing().xxs()).left(theme.spacing().xxs()),
-                        )
-                        .width(Length::Fill)
-                        .on_press(Message::ToggleMenu(layout.id)),
-                );
-
-                if is_open {
-                    content
-                        .push(Column::with_children(
-                            layout
-                                .children
-                                .iter()
-                                .map(|item| self.view_menu(&self.name, item)),
-                        ))
-                        .into()
-                } else {
-                    content.into()
-                }
+                button(button_content)
+                    .style(|_, status| {
+                        let base = button::Style {
+                            background: Some(Color::TRANSPARENT.into()),
+                            text_color: TEXT,
+                            ..Default::default()
+                        };
+                        match status {
+                            button::Status::Hovered => button::Style {
+                                background: Some(SURFACE0.into()),
+                                ..base
+                            },
+                            _ => base,
+                        }
+                    })
+                    .padding(padding::vertical(theme.spacing().xxs()).left(theme.spacing().xxs()))
+                    .width(Length::Fill)
+                    .on_press(Message::OpenSubMenu(layout.id))
+                    .into()
             }
             // regular button
             TrayLayoutProps {
@@ -211,10 +143,102 @@ impl MenuComp {
                     .into()
             }
             _ => {
-                // info!("empty");
+                info!("empty");
                 Row::new().into()
             }
         }
+    }
+}
+
+impl Comp for MenuComp {
+    type Message = Message;
+    type Init = Init;
+
+    fn new(input: Self::Init) -> Self {
+        let theme = app_theme();
+        let layout = &input.layout;
+        info!("layout: {layout:?}");
+        Self {
+            name: input.name,
+            layout: input.layout,
+            menu_stack: Vec::new(),
+            theme,
+        }
+    }
+
+    fn update(&mut self, message: Self::Message) -> iced::Task<Self::Message> {
+        match message {
+            Message::OpenSubMenu(id) => {
+                self.menu_stack.push(id);
+                Task::none()
+            }
+            Message::CloseSubMenu => {
+                self.menu_stack.pop();
+                Task::none()
+            }
+            Message::ItemSelected(_name, _id) => Task::none(),
+        }
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        let theme = &self.theme;
+        let menu_items = Self::get_current_menu(&self.menu_stack, &self.layout);
+
+        let mut top_menu = menu_items
+            .iter()
+            .map(|menu| {
+                Element::from(
+                    container(self.view_menu(&self.name, menu))
+                        .padding(padding::left(theme.spacing().xs()))
+                        .center_y(theme.spacing().lg())
+                        .center_x(Length::Fill),
+                )
+            })
+            .fold(Column::new(), |col, item_elem| col.push(item_elem));
+
+        if !self.menu_stack.is_empty() {
+            top_menu = top_menu.push({
+                let label = text("Back")
+                    .align_y(Vertical::Center)
+                    .size(theme.spacing().md());
+
+                Element::from(
+                    button(label)
+                        .style(|_, status| {
+                            let base = button::Style {
+                                background: Some(Color::TRANSPARENT.into()),
+                                text_color: TEXT,
+                                ..Default::default()
+                            };
+                            match status {
+                                button::Status::Hovered => button::Style {
+                                    background: Some(SURFACE0.into()),
+                                    ..base
+                                },
+                                _ => base,
+                            }
+                        })
+                        .padding(
+                            padding::left(theme.spacing().xl()).vertical(theme.spacing().xxs()),
+                        )
+                        .width(Length::Fill)
+                        .on_press(Message::CloseSubMenu),
+                )
+            })
+        }
+
+        container(top_menu)
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .padding(theme.spacing().xs())
+            .style(|_| container::Style {
+                border: border::rounded(theme.radius().lg())
+                    .color(theme.secondary(Shade::S500))
+                    .width(theme.spacing().xxs()),
+                background: Some(theme.neutral(Shade::S900).into()),
+                ..Default::default()
+            })
+            .into()
     }
 }
 
