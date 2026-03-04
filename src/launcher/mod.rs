008ea277f1ea50c94c83689abddf904559f5c1c0
Berkeley True <berkeleytrue@users.noreply.github.com>
2026-03-04 15:56:15 -0800

    fix(launcher): use fd entry parser, pull in exec

--- src/launcher/mod.rs
+++ src/launcher/mod.rs
@@ -1,10 +1,12 @@
 use std::{
     collections::{BTreeMap, BTreeSet},
+    env::VarError,
     ffi::OsStr,
     path::PathBuf,
 };
 
 use derive_more::{Constructor, Deref, DerefMut, Display, From};
+use freedesktop_entry_parser::{Entry, parse_entry};
 use iced::{
     Border, Event, Length, Task,
     alignment::Vertical,
@@ -16,7 +18,7 @@ use iced::{
 use iced_layershell::reexport::{
     Anchor, KeyboardInteractivity, Layer, NewLayerShellSettings, OutputOption,
 };
-use ini::ini;
+use itertools::Itertools;
 use tokio::fs;
 use tracing::info;
 
@@ -31,10 +33,11 @@ struct Application {
     exec: String,
     comment: Option<String>,
     try_exec: Option<String>,
+    icon: Option<String>,
 }
 
 #[derive(Debug, Deref, DerefMut, From)]
-struct PathToAppSet(BTreeMap<PathBuf, Application>);
+struct AppNameToAppMap(BTreeMap<String, Application>);
 
 #[derive(Clone, Debug, Display)]
 enum PromptType {
@@ -168,62 +171,95 @@ fn get_bin_dirs() -> anyhow::Result<BTreeSet<PathBuf>> {
     Ok(paths)
 }
 
-fn get_data_dirs() -> anyhow::Result<BTreeSet<PathBuf>> {
-    let data_dir: BTreeSet<_> = std::env::var("XDG_DATA_DIRS")?
+fn get_data_dirs() -> anyhow::Result<Vec<PathBuf>> {
+    let mut data_dir: Vec<_> = std::env::var("XDG_DATA_DIRS")
+        .or_else(|err| match err {
+            VarError::NotPresent => Ok("/usr/local/share:/usr/share".to_owned()),
+            _ => Err(err),
+        })?
         .split(":")
         .filter_map(|path_str| {
-            PathBuf::from(format!("{path_str}/application"))
+            PathBuf::from(format!("{path_str}/applications"))
                 .canonicalize()
                 .ok()
         })
+        .filter(|data_dir| data_dir.exists())
+        .dedup()
         .collect();
+
+    // initial data dirs have higher priority
+    data_dir.reverse();
 
     Ok(data_dir)
 }
 
-async fn get_apps() -> anyhow::Result<()> {
+async fn get_apps() -> anyhow::Result<AppNameToAppMap> {
     let paths = get_bin_dirs()?;
     let data_dirs = get_data_dirs()?;
 
-    let mut apps = PathToAppSet::from(BTreeMap::new());
+    let mut apps = AppNameToAppMap::from(BTreeMap::new());
 
     for data_dir in data_dirs.iter() {
         let mut entries = fs::read_dir(data_dir).await?;
         while let Some(entry) = entries.next_entry().await? {
             let path = entry.path();
-            let path_str = path.to_str();
-            if path.extension() == Some(OsStr::new("application"))
-                && let Some(path_str) = path_str
-            {
-                let desktop = ini!(safe path_str).map_err(|s| anyhow::anyhow!(s))?;
-                let desktop = desktop["Desktop Entry"].clone();
-                let is_app = desktop["Type"]
-                    .as_ref()
-                    .is_some_and(|tpe| tpe == "Application");
+            let file_name = path
+                .file_stem()
+                .and_then(|filename| filename.to_str())
+                .map(|s| s.to_owned())
+                .unwrap_or("na".to_owned());
 
-                let name = desktop["Name"].clone();
-                let exec = desktop["Exec"].clone();
+            if path.extension() == Some(OsStr::new("desktop")) {
+                let desktop = tokio::task::spawn_blocking(|| parse_entry(path)).await??;
+                let is_visible_app = desktop
+                    .get_desk_entry("Type")
+                    .is_some_and(|typo| typo == "Application")
+                    && desktop.get_desk_entry("Hidden").is_none_or(|s| s != "True")
+                    && desktop
+                        .get_desk_entry("NoDisplay")
+                        .is_none_or(|s| s != "True");
 
-                let try_exec = desktop["TryExec"].clone();
-                let comment = desktop["Comment"].clone();
-                if is_app
+                let name = desktop.get_desk_entry("Name");
+                let exec = desktop.get_desk_entry("Exec");
+
+                let try_exec = desktop.get_desk_entry("TryExec");
+                let comment = desktop.get_desk_entry("Comment");
+                let icon = desktop.get_desk_entry("Icon");
+
+                if is_visible_app
                     && let Some(name) = name
                     && let Some(exec) = exec
-                    && verify_exec(&exec, try_exec.as_ref(), &paths).await
+                    && verify_exec(exec, try_exec, &paths).await
                 {
-                    apps.insert(path, Application::new(name, exec, comment, try_exec));
+                    let exec = exec
+                        .split_whitespace()
+                        .filter(|token| !token.starts_with("%"))
+                        .collect::<Vec<_>>()
+                        .join(" ");
+
+                    apps.insert(
+                        file_name,
+                        Application::new(
+                            name.to_owned(),
+                            exec.to_owned(),
+                            comment.cloned(),
+                            try_exec.cloned(),
+                            icon.cloned(),
+                        ),
+                    );
                 }
             }
         }
     }
 
-    Ok(())
+    Ok(apps)
 }
 
 async fn verify_exec(exec: &String, try_exec: Option<&String>, paths: &BTreeSet<PathBuf>) -> bool {
     let maybe_exec = try_exec
         .unwrap_or(exec)
         .split_whitespace()
+        // grab first part, should be exec name or full path
         .next()
         .map(PathBuf::from);
 
@@ -235,5 +271,16 @@ async fn verify_exec(exec: &String, try_exec: Option<&String>, paths: &BTreeSet<
         }
     } else {
         false
+    }
+}
+
+trait EntryExt {
+    fn get_desk_entry(&self, attr: impl AsRef<str>) -> Option<&String>;
+}
+
+impl EntryExt for Entry {
+    fn get_desk_entry(&self, attr: impl AsRef<str>) -> Option<&String> {
+        self.get("Desktop Entry", attr)
+            .and_then(|entries| entries.first())
     }
 }
